60ae06658a3c9527513dbbcbf6c1a42ac4ce6f31
DharmaTurtle <DharmaTurtle@gmail.com>
2015-04-17 17:28:32 -0500

    Created Banner and Commander projects, added references to appropriate projects

---
+++ Dbot.Banner/Banner.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dbot.CommonModels;
+
+namespace Dbot.Banner {
+  public class Banner {
+    private Message _input;
+    public Banner(Message input) {
+      _input = input;
+    }
+
+    public Victim BanParser() {
+
+      return null;
+    }
+  }
+}
