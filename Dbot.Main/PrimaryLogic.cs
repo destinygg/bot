ae7532d180ca61417fb0b1f9b3e48baf332c607d
DharmaTurtle <DharmaTurtle@gmail.com>
2015-05-21 20:56:43 -0500

    Continued removing code from Main(). Now it has only 1 line, and the rest is housed in the PrimaryLogic class

---
+++ Dbot.Main/PrimaryLogic.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dbot.Common;
+using Dbot.Data;
+using Dbot.Processor;
+using Dbot.Utility;
+using Dbot.WebsocketClient;
+using Tweetinvi;
+using Tweetinvi.Core.Interfaces.Streaminvi;
+using Tweetinvi.Streams;
+
+namespace Dbot.Main {
+  public class PrimaryLogic {
+    private static readonly IClient Client = new WebSocketClient();
+    private static readonly IProcessor Processor = new MessageProcessor(Client);
+    private static readonly IUserStream UserStream = Stream.CreateUserStream();
+    private static bool _exit;
+
+    public void Run() {
+      InitializeDatastore.Run();
+
+      TwitterCredentials.SetCredentials(PrivateConstants.Twitter_Access_Token,
+      PrivateConstants.Twitter_Access_Token_Secret, PrivateConstants.Twitter_Consumer_Key,
+      PrivateConstants.Twitter_Consumer_Secret);
+      UserStream.TweetCreatedByFriend += (sender, args) => TweetDetected(args.Tweet.Text);
+      UserStream.StartStreamAsync();
+
+      //todo, make sure this dosn't run more often than once a minute
+      PeriodicTask.Run(() => Tools.LiveStatus(), TimeSpan.FromMinutes(2));
+      PeriodicTask.Run(InitializeDatastore.UpdateEmoticons, TimeSpan.FromHours(1));
+
+      Client.Run(Processor);
+      Console.CancelKeyPress += Console_CancelKeyPress;
+      //http://stackoverflow.com/questions/14255655/tpl-dataflow-producerconsumer-pattern
+      //http://msdn.microsoft.com/en-us/library/hh228601(v=vs.110).aspx
+
+      while (!_exit) {
+        //Process.GetCurrentProcess().WaitForExit(); // If you ever have to get rid of the while(true)
+        var input = Console.ReadLine();
+        if (!String.IsNullOrEmpty(input)) {
+          if (input == "exit") {
+            _exit = true;
+          } else if (input[0] == '~') {
+            Client.Send(Make.Message(input.Substring(1)));
+          } else if (input[0] == '<') {
+            MessageProcessor.ModCommander.Post(Make.Message(input));
+          }
+        }
+      }
+
+      Exit();
+    }
+
+    private static void TweetDetected(string tweet) {
+      MessageProcessor.Sender.Post(Make.Message("twitter.com/steven_bonnell just tweeted: " + tweet));
+    }
+
+    static void Console_CancelKeyPress(object sender, ConsoleCancelEventArgs e) {
+      Exit();
+    }
+
+    static void Exit() {
+      Datastore.Terminate();
+    }
+  }
+}
--- Dbot.Main/Program.cs
+++ Dbot.Main/Program.cs
@@ -16,55 +16,10 @@ using Tweetinvi;
 using Tweetinvi.Core.Interfaces.Streaminvi;
 
 namespace Dbot.Main {
-  static class Dbot {
-    private static readonly IClient Client = new WebSocketClient();
-    private static readonly IProcessor Processor = new MessageProcessor(Client);
-    private static readonly IUserStream UserStream = Stream.CreateUserStream();
-    private static bool _exit;
+  class Dbot {
 
     static void Main() {
-
-      InitializeDatastore.Run();
-
-      TwitterCredentials.SetCredentials(PrivateConstants.Twitter_Access_Token, PrivateConstants.Twitter_Access_Token_Secret, PrivateConstants.Twitter_Consumer_Key, PrivateConstants.Twitter_Consumer_Secret);
-      UserStream.TweetCreatedByFriend += (sender, args) => TweetDetected(args.Tweet.Text);
-      UserStream.StartStreamAsync();
-
-      //todo, make sure this dosn't run more often than once a minute
-      PeriodicTask.Run(() => Tools.LiveStatus(), TimeSpan.FromMinutes(2));
-      PeriodicTask.Run(InitializeDatastore.UpdateEmoticons, TimeSpan.FromHours(1));
-
-      Client.Run(Processor);
-      Console.CancelKeyPress += Console_CancelKeyPress;
-      //http://stackoverflow.com/questions/14255655/tpl-dataflow-producerconsumer-pattern
-      //http://msdn.microsoft.com/en-us/library/hh228601(v=vs.110).aspx
-
-      while (!_exit) { //Process.GetCurrentProcess().WaitForExit(); // If you ever have to get rid of the while(true)
-        var input = Console.ReadLine();
-        if (!String.IsNullOrEmpty(input)) {
-          if (input == "exit") {
-            _exit = true;
-          } else if (input[0] == '~') {
-            Client.Send(Make.Message(input.Substring(1)));
-          } else if (input[0] == '<') {
-            MessageProcessor.ModCommander.Post(Make.Message(input));
-          }
-        }
-      }
-
-      Exit();
-    }
-
-    private static void TweetDetected(string tweet) {
-      MessageProcessor.Sender.Post(Make.Message("twitter.com/steven_bonnell just tweeted: " + tweet));
-    }
-
-    static void Console_CancelKeyPress(object sender, ConsoleCancelEventArgs e) {
-      Exit();
-    }
-
-    static void Exit() {
-      Datastore.Terminate();
+      new PrimaryLogic().Run();
     }
   }
 }
