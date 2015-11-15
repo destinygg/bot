2e7a1876484902e34e08d12a1e5548bd15bca66e
DharmaTurtle <DharmaTurtle@gmail.com>
2015-11-15 11:28:09 -0600

    Updated SampleClient to take raw copy/pasted text from IRC.

--- Dbot.Client/SampleClient.cs
+++ Dbot.Client/SampleClient.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using Dbot.Common;
@@ -11,12 +12,14 @@ using Dbot.Utility;
 namespace Dbot.Client {
   public class SampleClient : ConsolePrintClient {
     private IProcessor _processor;
+    private readonly List<Message> _messageList = new List<Message>();
 
     public override async void Run(IProcessor processor) {
       _processor = processor;
+      ParseRawIrc();
       foreach (var message in _messageList) {
         processor.ProcessMessage(message);
-        await Task.Run(() => Task.Delay(0));
+        await Task.Run(() => Task.Delay(100));
       }
     }
 
@@ -24,29 +27,25 @@ namespace Dbot.Client {
       _processor.ProcessMessage(message);
     }
 
-    private readonly List<Message> _messageList = new List<Message> {
-      new PublicMessage("a", "abcdefghijklmnopqrstuvwxyz"),
-      new PublicMessage("b", "abcdefghijklmnopqrstuvwxyz"),
-      new PublicMessage("c", "abcdefghijklmnopqrstuvwxyz"),
-      new PublicMessage("d", "abcdefghijklmnopqrstuvwxyz"),
-      new PublicMessage("e", "abcdefghijklmnopqrstuvwxyz"),
-      new PublicMessage("f", "abcdefghijklmnopqrstuvwxyz"),
-      new PublicMessage("g", "abcdefghijklmnopqrstuvwxyz"),
-      new PublicMessage("h", "abcdefghijklmnopqrstuvwxyz"),
-      new PublicMessage("i", "abcdefghijklmnopqrstuvwxyz"),
-      new PublicMessage("j", "abcdefghijklmnopqrstuvwxyz"),
-      new PublicMessage("k", "abcdefghijklmnopqrstuvwxyz"),
-      new PublicMessage("l", "abcdefghijklmnopqrstuvwxyz"),
-      new PublicMessage("m", "abcdefghijklmnopqrstuvwxyz"),
-      new PublicMessage("n", "abcdefghijklmnopqrstuvwxyz"),
-      new PublicMessage("o", "abcdefghijklmnopqrstuvwxyz"),
-      new PublicMessage("p", "abcdefghijklmnopqrstuvwxyz"),
-      new PublicMessage("q", "abcdefghijklmnopqrstuvwxyz"),
-      new PublicMessage("r", "abcdefghijklmnopqrstuvwxyz"),
-      new PublicMessage("s", "abcdefghijklmnopqrstuvwxyz"),
-      new PublicMessage("t", "abcdefghijklmnopqrstuvwxyz"),
-      new PublicMessage("u", "abcdefghijklmnopqrstuvwxyz"),
-      new PublicMessage("u", "!time"),
-    };
+    private void ParseRawIrc() {
+      var regex = new Regex(@"<II> <(\S+)> (.*)");
+      var lines = RawIrc.Split(new[] { "\r\n", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+      foreach (var line in lines) {
+        var match = regex.Match(line);
+        if (match.Success) {
+          var nick = match.Groups[1].Value;
+          var text = match.Groups[2].Value;
+          if (nick == "Bot") {
+            //_messageList.Add(new ModPublicMessage(text));
+          } else {
+            _messageList.Add(new PublicMessage(nick, text));
+          }
+        }
+      }
+    }
+
+    private const string RawIrc = @"
+
+";
   }
 }
