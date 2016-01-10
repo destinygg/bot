b030f5f974389d66811268ae0936fad952ecec1c
DharmaTurtle <DharmaTurtle@gmail.com>
2016-01-09 20:07:52 -0600

    Added commentary about `dynamic` and visitor pattern.

--- Dbot.CommonModels/IClientVisitor.cs
+++ Dbot.CommonModels/IClientVisitor.cs
@@ -1,4 +1,7 @@
-﻿namespace Dbot.CommonModels {
+﻿// http://code.logos.com/blog/2010/03/the_visitor_pattern_and_dynamic_in_c_4.html
+// Considered using `dynamic` to eliminate visitor pattern, but rejected due to the potential for runtime errors
+
+namespace Dbot.CommonModels {
   public interface IClientVisitor {
     void Run(IProcessor processor);
     void Forward(PublicMessage message);
