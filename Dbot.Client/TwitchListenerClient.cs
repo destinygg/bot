69e188afb3aa0ffed9476551e5a98adb182c8762
DharmaTurtle <DharmaTurtle@gmail.com>
2016-01-24 10:09:18 -0600

    Dropping static

--- Dbot.Client/TwitchListenerClient.cs
+++ Dbot.Client/TwitchListenerClient.cs
@@ -49,7 +49,7 @@ namespace Dbot.Client {
       Client.Channels.Join("#dharmaturtle");
     }
 
-    private static void IrcClient_Registered(object sender, EventArgs e) {
+    private void IrcClient_Registered(object sender, EventArgs e) {
       var client = (IrcClient) sender;
 
       client.LocalUser.NoticeReceived += IrcClient_LocalUser_NoticeReceived;
@@ -58,7 +58,7 @@ namespace Dbot.Client {
       client.LocalUser.LeftChannel += IrcClient_LocalUser_LeftChannel;
     }
 
-    private static void IrcClient_LocalUser_LeftChannel(object sender, IrcChannelEventArgs e) {
+    private void IrcClient_LocalUser_LeftChannel(object sender, IrcChannelEventArgs e) {
       var localUser = (IrcLocalUser) sender;
 
       e.Channel.UserJoined -= IrcClient_Channel_UserJoined;
@@ -69,7 +69,7 @@ namespace Dbot.Client {
       Console.WriteLine("You left the channel {0}.", e.Channel.Name);
     }
 
-    private static void IrcClient_LocalUser_JoinedChannel(object sender, IrcChannelEventArgs e) {
+    private void IrcClient_LocalUser_JoinedChannel(object sender, IrcChannelEventArgs e) {
       var localUser = (IrcLocalUser) sender;
 
       e.Channel.UserJoined += IrcClient_Channel_UserJoined;
@@ -80,13 +80,13 @@ namespace Dbot.Client {
       Console.WriteLine("You joined the channel {0}.", e.Channel.Name);
     }
 
-    private static void IrcClient_Channel_NoticeReceived(object sender, IrcMessageEventArgs e) {
+    private void IrcClient_Channel_NoticeReceived(object sender, IrcMessageEventArgs e) {
       var channel = (IrcChannel) sender;
 
       Console.WriteLine("[{0}] Notice: {1}.", channel.Name, e.Text);
     }
 
-    private static void IrcClient_Channel_MessageReceived(object sender, IrcMessageEventArgs e) {
+    private void IrcClient_Channel_MessageReceived(object sender, IrcMessageEventArgs e) {
       var channel = (IrcChannel) sender;
       if (e.Source is IrcUser) {
         // Read message.
@@ -96,17 +96,17 @@ namespace Dbot.Client {
       }
     }
 
-    private static void IrcClient_Channel_UserLeft(object sender, IrcChannelUserEventArgs e) {
+    private void IrcClient_Channel_UserLeft(object sender, IrcChannelUserEventArgs e) {
       var channel = (IrcChannel) sender;
       Console.WriteLine("[{0}] User {1} left the channel.", channel.Name, e.ChannelUser.User.NickName);
     }
 
-    private static void IrcClient_Channel_UserJoined(object sender, IrcChannelUserEventArgs e) {
+    private void IrcClient_Channel_UserJoined(object sender, IrcChannelUserEventArgs e) {
       var channel = (IrcChannel) sender;
       Console.WriteLine("[{0}] User {1} joined the channel.", channel.Name, e.ChannelUser.User.NickName);
     }
 
-    private static void IrcClient_LocalUser_MessageReceived(object sender, IrcMessageEventArgs e) {
+    private void IrcClient_LocalUser_MessageReceived(object sender, IrcMessageEventArgs e) {
       var localUser = (IrcLocalUser) sender;
 
       if (e.Source is IrcUser) {
@@ -117,16 +117,16 @@ namespace Dbot.Client {
       }
     }
 
-    private static void IrcClient_LocalUser_NoticeReceived(object sender, IrcMessageEventArgs e) {
+    private void IrcClient_LocalUser_NoticeReceived(object sender, IrcMessageEventArgs e) {
       var localUser = (IrcLocalUser) sender;
       Console.WriteLine("Notice: {0}.", e.Text);
     }
 
-    private static void IrcClient_Disconnected(object sender, EventArgs e) {
+    private void IrcClient_Disconnected(object sender, EventArgs e) {
       var client = (IrcClient) sender;
     }
 
-    private static void IrcClient_Connected(object sender, EventArgs e) {
+    private void IrcClient_Connected(object sender, EventArgs e) {
       var client = (IrcClient) sender;
     }
 
