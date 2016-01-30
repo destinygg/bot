da615e6514b6477bf98b73f63cc86a646d148a21
DharmaTurtle <DharmaTurtle@gmail.com>
2016-01-30 12:22:38 -0600

    Stopped following the MS tutorial and instead trying one from CodeProject. Removed the Designer and added PrimaryServiceInstaller
    http://www.codeproject.com/Articles/14353/Creating-a-Basic-Windows-Service-in-C

---
+++ Dbot.Service/PrimaryServiceInstaller.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dbot.Service {
+  [RunInstaller(true)]
+  public class PrimaryServiceInstaller : Installer {
+    /// <summary>
+    /// Public Constructor for WindowsServiceInstaller.
+    /// - Put all of your Initialization code here.
+    /// </summary>
+    public PrimaryServiceInstaller() {
+      ServiceProcessInstaller serviceProcessInstaller =
+                         new ServiceProcessInstaller();
+      ServiceInstaller serviceInstaller = new ServiceInstaller();
+
+      //# Service Account Information
+      serviceProcessInstaller.Account = ServiceAccount.LocalSystem;
+      serviceProcessInstaller.Username = null;
+      serviceProcessInstaller.Password = null;
+
+      //# Service Information
+      serviceInstaller.DisplayName = "My New C# Windows Service";
+      serviceInstaller.StartType = ServiceStartMode.Automatic;
+
+      //# This must be identical to the WindowsService.ServiceBase name
+      //# set in the constructor of WindowsService.cs
+      serviceInstaller.ServiceName = "My Windows Service 1";
+
+      this.Installers.Add(serviceProcessInstaller);
+      this.Installers.Add(serviceInstaller);
+    }
+  }
+}
