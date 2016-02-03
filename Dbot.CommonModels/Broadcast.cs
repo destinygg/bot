410a660fb9937f639eda01e8b3a8734c8af6033f
DharmaTurtle <DharmaTurtle@gmail.com>
2016-02-02 21:30:54 -0600

    Created Broadcast

---
+++ Dbot.CommonModels/Broadcast.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dbot.CommonModels {
+  public class Broadcast : Message {
+    public Broadcast(string nick, string originalText) : base(nick, originalText) {
+      this.Nick = string.IsNullOrEmpty(nick) ? "CHANNEL" : nick;
+      this.OriginalText = originalText;
+    }
+
+    public override void Accept(IClientVisitor visitor) {
+      visitor.Visit(this);
+    }
+  }
+}
