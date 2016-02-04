2b51a7fa2e292c4fb4b0d71e3c8ae638e4c9a00c
DharmaTurtle <DharmaTurtle@gmail.com>
2016-02-04 11:52:58 -0600

    Created ConnectedUsers which takes and contains a list of users.

---
+++ Dbot.CommonModels/ConnectedUsers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dbot.CommonModels {
+  public class ConnectedUsers {
+    public ConnectedUsers(List<User> users) {
+      Users = users;
+    }
+
+    public List<User> Users { get; set; }
+  }
+}
