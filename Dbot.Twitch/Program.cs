7011d1b031a1a67e8de61876ed9f832ba7c007cb
DharmaTurtle <DharmaTurtle@gmail.com>
2016-02-08 15:09:17 -0600

    Made Main actually connect to Twitch

--- Dbot.Twitch/Program.cs
+++ Dbot.Twitch/Program.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dbot.Client;
+using Dbot.Utility;
 
 namespace Dbot.Twitch {
   class Program {
-    static void Main(string[] args) {
+    static void Main() {
+      Logger.Init();
+      //var server = "irc.rizon.net";
+      const string server = "irc.twitch.tv";
+      const int port = 6667;
+      const string nick = "destiny_bot";
+      const string pass = PrivateConstants.TwitchOauth;
+      using (var bot = new SimpleIrcClient(server, port, nick, pass)) {
+        bot.Connect();
+        bot.Run();
+      }
     }
   }
 }
