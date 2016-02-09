225e17d1000ce9f2fcf76238c13394719dd4b954
DharmaTurtle <DharmaTurtle@gmail.com>
2016-02-08 22:40:59 -0600

    Created SimpleIrcClient, which extends SimpleIrcListenerClient

---
+++ Dbot.Client/SimpleIrcClient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dbot.CommonModels;
+
+namespace Dbot.Client {
+  public class SimpleIrcClient : SimpleIrcListenerClient {
+    public SimpleIrcClient(string server, int port, string channel, string nick, string pass = null) : base(server, port, channel, nick, pass) { }
+
+    
+  }
+}
