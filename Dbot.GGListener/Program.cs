c346cdd340af21f67304e8988522ed96c409d07f
DharmaTurtle <DharmaTurtle@gmail.com>
2016-03-04 00:44:44 -0600

    Created Dbot.GGListener, calling WebSocketListenerClient and its logic

--- Dbot.GGListener/Program.cs
+++ Dbot.GGListener/Program.cs
@@ -2,10 +2,10 @@
 using Dbot.Logic;
 using Dbot.Utility;
 
-namespace Dbot.GG {
+namespace Dbot.GGListener {
   class Program {
     static void Main(string[] args) {
-      var client = new WebSocketListenerClient(PrivateConstants.BotWebsocketAuth);
+      var client = new WebSocketClient(PrivateConstants.BotWebsocketAuth);
       new PrimaryLogic(client).Run();
     }
   }
